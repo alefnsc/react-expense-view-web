933b44bd664dca8b737917e838106f263716412b
Alexandre Augusto Fonseca <alexandralexandrefonsecach@gmail.com>
2023-11-19 17:33:49 -0300

    Vercel deploy fix test

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,30 +1,18 @@
-// See https://github.com/typicode/json-server#module
 import jsonServer from "json-server";
-
-export const server = jsonServer.create();
-
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-const router = jsonServer.router("db.json");
-
+const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
-server.use(router);
+
+// Add custom routes for "despesas" only
+const despesasRouter = jsonServer.router("db.json", {
+  foreignKeySuffix: "_id",
+});
+server.use("/despesas", despesasRouter);
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+// Assign the server object to a global variable
+window.server = server;
